d6772d341f5e19bd1a418fd6153f83d0eea439f8
Méwen DORAFI <dorafimewen@pm.me>
2023-04-07 11:07:19 +0200

    first commit

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+const connectionString = 'mongodb+srv://la-capsule:yisdf1HUmykN40CX@cluster0.bfcvn.mongodb.net/weatherapp?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
